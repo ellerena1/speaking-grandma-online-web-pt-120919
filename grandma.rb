137f10307071a430a1a451776aa52a8f0078c387
Esteban S Llerena <estebanllerena@gmail.com>
2019-12-14 05:58:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 
 def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA"
+  if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
     elsif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
